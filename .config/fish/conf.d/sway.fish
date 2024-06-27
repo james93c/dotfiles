3afe2b41ba3c2f980976363cefbc71c3eb2f3426
james93c <james93cotter@gmail.com>
2024-06-27 17:15:47 +0100

    color schemes

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -1,3 +1,8 @@
 # If running from tty1 start sway
 set TTY1 (tty)
-[ "$TTY1" = "/dev/tty1" ] && exec sway
+if [ "$TTY1" = "/dev/tty1" ]
+    set -Ux COLORSCHEME "sonokai"
+    ln -sf "/home/james/.config/sway/$COLORSCHEME.conf" "/home/james/.config/sway/colorscheme.conf"
+    ln -sf "/home/james/.config/foot/$COLORSCHEME.ini" "/home/james/.config/foot/colorscheme.ini" 
+    exec sway
+end
