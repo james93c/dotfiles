516445dc0413c0de177c784261409f7d0f90cf12
james93c <james93cotter@gmail.com>
2024-06-27 13:28:44 +0100

    comment t function. separate out color schemes from foot/sway configs.

--- .config/fish/functions/t.fish
+++ .config/fish/functions/t.fish
@@ -1,26 +1,35 @@
 function t
-  if test (count $argv) -lt 1
-   set selected (find ~/dev -mindepth 1 -maxdepth 2 -type d | fzf)
-  else
-    set selected $argv[1]
-  end
+    # if no args are supplied, use fzf to choose a directory
+    if test (count $argv) -lt 1
+        set selected (find ~/dev -mindepth 1 -maxdepth 2 -type d | fzf)
+    else
+        set selected $argv[1]
+    end
 
-  if test -z "$selected"
-      exit 0
-  end
+    # exit if no path selected
+    if test -z "$selected"
+        return
+    end
 
-  set selected_name (basename "$selected" | tr . _)
-  set tmux_running (pgrep tmux)
+    set selected_name (basename "$selected" | tr . _)
+    set tmux_running (pgrep tmux)
 
-  if test -z "$TMUX" -a -z "$tmux_running"
-      tmux new-session -s $selected_name -c $selected
-      exit 0
-  end
+    # check if same session already exists
+    if not tmux has-session -t=$selected_name 2> /dev/null
+        tmux new-session -ds $selected_name -c $selected
+    end
 
-  if not tmux has-session -t=$selected_name ^/dev/null
-      tmux new-session -ds $selected_name -c $selected
-  end
+    # if this is not a tmux session, attach to the session, else switch client
+    if test -z "$TMUX"
+        tmux attach -dt $selected_name
+        exit 0
+    else
+        set current_session (tmux display-message -p '#S')
+        if test $selected_name != $current_session
+            tmux switch-client -t $selected_name
+            tmux kill-session -t $current_session
+        end
+    end
 
-  tmux switch-client -t $selected_name
 end
 
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -119,22 +119,21 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        builtin.find_files { hidden = true }
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>so', builtin.oldfiles, { desc = '[S]earch [O]ld Files' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
+        builtin.current_buffer_fuzzy_find()
       end, { desc = '[/] Fuzzily search in current buffer' })
 
       -- It's also possible to pass additional configuration options.
@@ -488,20 +487,52 @@ require('lazy').setup({
       end)
     end,
   },
-
   {
-    --  'ellisonleao/gruvbox.nvim',
-    'markvincze/panda-vim',
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    -- init = function()
+    --   vim.cmd.colorscheme 'kanagawa'
+    -- end,
+  },
+  {
+    'sainnhe/sonokai',
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'panda'
-      -- vim.cmd.hi 'Comment gui=none'
-      -- vim.cmd.highlight 'Normal guibg=#292A2C'
-      -- vim.cmd.highlight 'Visual guibg=#3C3D3E'
-      vim.cmd.highlight 'NormalFloat guibg=#333539'
+      -- vim.g.sonokai_style = 'shusia'
+      vim.g.sonokai_better_performance = true
+      vim.g.sonokai_enable_italic = true
+      vim.cmd.colorscheme 'sonokai'
     end,
   },
-
+  {
+    'rose-pine/neovim',
+    priority = 1000,
+    -- init = function()
+    --   vim.cmd.colorscheme 'rose-pine'
+    -- end,
+  },
+  {
+    'catppuccin/nvim',
+    priority = 1000,
+    -- init = function()
+    --   vim.cmd.colorscheme 'catppuccin'
+    -- end,
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    -- init = function()
+    --   vim.cmd.colorscheme 'gruvbox'
+    -- end,
+  },
+  {
+    'markvincze/panda-vim',
+    priority = 1000,
+    -- init = function()
+    --   vim.cmd.colorscheme 'panda'
+    --   vim.cmd.highlight 'NormalFloat guibg=#333539'
+    -- end,
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   {
