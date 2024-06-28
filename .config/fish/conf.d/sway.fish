49ca48ac2840f3e9d2a70e9acae9f760da60f653
james93c <james93cotter@gmail.com>
2024-06-28 11:14:09 +0100

    export colors as env vars

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -2,7 +2,9 @@
 set TTY1 (tty)
 if [ "$TTY1" = "/dev/tty1" ]
     set -Ux COLORSCHEME "sonokai"
+    exportcolors
     ln -sf "/home/james/.config/sway/$COLORSCHEME.conf" "/home/james/.config/sway/colorscheme.conf"
     ln -sf "/home/james/.config/foot/$COLORSCHEME.ini" "/home/james/.config/foot/colorscheme.ini" 
+    ln -sf "/home/james/Pictures/Wallpapers/$COLORSCHEME.png" "/home/james/Pictures/Wallpapers/wallpaper.png" 
     exec sway
 end
